5f577c7de7b6cebb2d0b6b8bac1ddd7f0ae6f2e1
jsmoot93 <47538435+jsmoot93@users.noreply.github.com>
2019-03-22 14:57:09 -0400

    styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">A long time ago in a galaxy far, far away...</h1>
         <StarwarsCharacters 
           starwarsChars={this.state.starwarsChars}
         />
